d0206dbd25c9c6c1d96d246cf08aef636043e50b
root <root@teamrot.fi>
2023-04-10 10:25:47 +0000

    Update

--- xmlrpc.php
+++ xmlrpc.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=/?simply_static_page=2327">
+		<meta http-equiv="refresh" content="0;url=/?simply_static_page=2743">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "/?simply_static_page=2327";
+			window.location = "/?simply_static_page=2743";
 		</script>
 
-		<p>You are being redirected to <a href="/?simply_static_page=2327">/?simply_static_page=2327</a></p>
+		<p>You are being redirected to <a href="/?simply_static_page=2743">/?simply_static_page=2743</a></p>
 	</body>
 </html>
